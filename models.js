d6ec6f0e6326e32e801f398c7e8dec6aec631a4e
Edgar <exgc12@gmail.com>
2017-07-10 19:46:01 -0400

    initial commit

---
+++ models.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+  name: {
+    firstName: {type: String, default: ""},
+    lastName: {type: String, default: ""}
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  contacts: []
+});
+
+userSchema.virtual('fullName').get(function() {
+  return `${this.author.firstName} ${this.author.lastName}`.trim();
+});
