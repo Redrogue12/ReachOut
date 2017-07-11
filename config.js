4857f02f3ae32ad839926cedf8a65d3af70ef329
Edgar <exgc12@gmail.com>
2017-07-11 17:05:56 -0400

    defined database name

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
-                       'mongodb://localhost/something';
+                       'mongodb://localhost/reachDB';
 exports.TEST_DATABASE_URL = (
   process.env.TEST_DATABASE_URL ||
   'mongodb://localhost/test-app'
