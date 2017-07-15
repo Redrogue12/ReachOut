97f2f1863190080a6281ec0e55bf9a0b8a864628
Edgar <exgc12@gmail.com>
2017-07-14 21:11:10 -0400

    init

---
+++ users/models.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+mongoose.Promise = global.Promise;
+
+const userSchema = mongoose.Schema({
+
+  firstName: {
+    type: String,
+    default: ""
+  },
+  lastName: {
+    type: String,
+    default: ""
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  contacts: []
+});
+
+userSchema.virtual('fullName').get(function() {
+  return `${this.firstName} ${this.lastName}`.trim();
+});
+
+userSchema.methods.apiRepr = function() {
+  return {
+    username: this.username,
+    name: this.fullName
+  };
+}
+
+userSchema.methods.validatePassword = function(password) {
+  return bcrypt.compare(password, this.password);
+}
+
+userSchema.statics.hashPassword = function(password) {
+  console.log("anything");
+  return bcrypt.hash(password, 10);
+}
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = {User};
---
+++ users/router.js
@@ -0,0 +1,128 @@
+const {BasicStrategy} = require('passport-http');
+const express = require('express');
+const jsonParser = require('body-parser').json();
+const passport = require('passport');
+
+const{User} = require('./models');
+
+const router = express.Router();
+
+router.use(jsonParser);
+
+router.use(passport.initialize());
+
+// BASIC STRATEGY
+const basicStrategy = new BasicStrategy((username, password, callback) => {
+  let user;
+  User
+    .findOne({username: username})
+    .exec()
+    .then(_user => {
+      user = _user;
+      if (!user) {
+        return callback(null, false);
+      }
+      return user.validatePassword(password);
+    })
+    .then(isValid => {
+      if (!isValid) {
+        return callback(null, false);
+      }
+      else {
+        return callback(null, user);
+      }
+    })
+    .catch(err => callback(err));
+});
+
+passport.use(basicStrategy);
+
+// Users POST endpoint
+router.post('/', (req, res) => {
+  if (!req.body) {
+    return res.status(400).json({message: 'No request body'});
+  }
+
+  if (!('username' in req.body)) {
+    return res.status(422).json({message: 'Missing field: username'});
+  }
+
+  let {firstName, lastName, username, password} = req.body;
+
+  if (typeof username !== 'string') {
+    return res.status(422).json({message: 'Incorrect field type: username'});
+  }
+
+  username = username.trim();
+
+  if (username === '') {
+    return res.status(422).json({message: 'Incorrect field length: username'});
+  }
+
+  if (!(password)) {
+    return res.status(422).json({message: 'Missing field: password'});
+  }
+
+  if (typeof password !== 'string') {
+    return res.status(422).json({message: 'Incorrect field type: password'});
+  }
+
+  password = password.trim();
+
+  if (password === '') {
+    return res.status(422).json({message: 'Incorrect field length: password'});
+  }
+
+  console.log(firstName, lastName, username, password);
+
+  // check for existing user
+  return User
+    .find({username})
+    .count()
+    .exec()
+    .then(count => {
+      if (count > 0) {
+        return res.status(422).json({message: 'username already taken'});
+      }
+      // if no existing user, hash password
+
+      return User.hashPassword(password);
+    })
+    .then(hash => {
+      console.log(hash);
+      return User
+        .create({
+          firstName: firstName,
+          lastName: lastName,
+          username: username,
+          password: hash
+        })
+    })
+    .then(user => {
+      return res.status(201).json(user.apiRepr());
+    })
+    .catch(err => {
+      res.status(500).json({message: 'Internal server error'})
+    });
+});
+
+// Users GET ENDPOINTS
+router.get('/', (req, res) => {
+  User
+    .find()
+    .exec()
+    .then(users => {
+      res.json(users.map(user => user.apiRepr()));
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({error: 'GET error'});
+    });
+});
+
+router.get('/me',
+  passport.authenticate('basic', {session: false}),
+  (req, res) => res.json({user: req.user.apiRepr()})
+);
+
+module.exports = {router};
